90b3291207d347401a92c207655164478f582551
Sebastian Martin Pino Ruiz <pinoruizsebastian@gmail.com>
2019-01-21 17:39:29 -0500

    21/01/2019 --> Ordenando Archivos

---
+++ Banana/Banana/Funciones/Funciones.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Banana.Funciones
+{
+    public class Funciones
+    {
+        public  string Path()
+        {
+            return  "http://" + HttpContext.Current.Request.Url.Host+ ":" + HttpContext.Current.Request.Url.Port;
+        }
+        public string Idioma()
+        {
+            XDocument xmlConfig = XDocument.Load(new Banana.Funciones.Funciones().Path() + "/General/ServerConfiguracion.xml", LoadOptions.None);
+            XElement RaizServer = xmlConfig.Element("Servers");
+            IEnumerable<XElement> Lenguaje = RaizServer.Descendants("Configuracion");
+
+            foreach (XElement Idioma in Lenguaje)
+            {
+                return Idioma.Element("Idioma").Value;
+            }
+            return "ES";
+        }
+        public string TranslateText(string varTexto, string idiomaZona)
+        {
+            string nombreIdioma = "";
+            string frase = "";
+            string varLocal = "";
+            XDocument xmlIdioma = XDocument.Load(new Banana.Funciones.Funciones().Path() + "/Plugins/Idiomas.xml", LoadOptions.None);
+            XElement RaizIdioma = xmlIdioma.Element("IdiomasP");
+            IEnumerable<XElement> Idiomas = RaizIdioma.Descendants("Idioma");
+            foreach (XElement Idioma in Idiomas)
+            {
+                nombreIdioma = Idioma.Element("Region").Value;
+                varLocal = Idioma.Element("Var").Value;
+                if (nombreIdioma == idiomaZona)
+                {
+                    if (varTexto == varLocal)
+                    {
+                        frase = Idioma.Element("Frase").Value;
+                        return frase;
+                    }
+                    return varTexto;
+                }
+            }
+            return varTexto;
+          
+        }
+    }
+}
---
+++ Banana/Banana/General/SqlServer/SQL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace SQL
+{
+    
+    public static class SQL
+    {
+        public static string CadenaSql()
+        {
+            XDocument xmlConfig = XDocument.Load(new Banana.Funciones.Funciones().Path() + "/General/ServerConfiguracion.xml", LoadOptions.None);
+            XElement RaizServer = xmlConfig.Element("Servers");
+            IEnumerable<XElement> Lenguaje = RaizServer.Descendants("SqlConfig");
+
+            foreach (XElement Idioma in Lenguaje)
+            {
+                return Idioma.Element("Cadena").Value;
+            }
+            return "null";
+        }
+
+        public static SqlConnection cnn = new SqlConnection(CadenaSql());
+        public static string Open()
+        {
+            try
+            {
+                if (cnn.State == ConnectionState.Closed)
+                {
+                    cnn.Open();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+            return "";
+        }
+        public static string Close()
+        {
+            try
+            {
+                if (cnn.State == ConnectionState.Open)
+                {
+                    cnn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+            return "";
+        }
+    }
+
+   
+}
